8621603fd8a1df21b7864b9667c261095377988e
Simon <simon.kwong64@gmail.com>
2025-04-28 17:55:57 -0700

    fix resume view

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,7 +1,7 @@
 export default function Resume() {
   return (
-    <div>
-      <iframe src='/Resume.pdf' height='100%' width='100%' />
+    <div className="h-screen">
+      <iframe src='/Resume.pdf' className="w-full h-full border-none" />
     </div>
   )
 }
